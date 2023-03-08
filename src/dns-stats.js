bc815947f5f14229f460e9d2b0bdd0634cd5358a
DmitriyIshchenko <to.dmitry.ishchenko@gmail.com>
2023-03-08 10:15:46 +0300

    feat: complete dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const stats = {};
+
+  domains.forEach((domain) => {
+    let str = "";
+    domain
+      .split(".")
+      .reverse()
+      .forEach((item) => {
+        str += `.${item}`;
+        stats[str] = stats[str] ? stats[str] + 1 : 1;
+      });
+  });
+
+  return stats;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
